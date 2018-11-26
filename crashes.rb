201fbd90827bc1fec0b96c07f1bbfc0f99adb27c
Nolan Neeley <nolanneeley@gmail.com>
2018-11-26 11:52:31 -0600

    initial commit

---
+++ crashes.rb
@@ -0,0 +1,7 @@
+# Link to Data Portal
+  # https://data.cityofchicago.org/Transportation/Traffic-Crashes-Crashes/85ca-t3if
+
+# Get a list of all crashes at the corner of Halsted and Hubbard.
+
+# Use the Windy Module:
+  # https://github.com/Chicago/windy
